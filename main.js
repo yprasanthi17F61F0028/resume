6615e8405b8d4239ca7bf58effc5da5f7f7ef2b7
yprasanthi17f61f0028 <yprasanthi777@gmail.com>
2018-08-01 16:11:53 +0530

    last

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ fetchedData.then(data=>{
   console.log(data);
   career(data.career);
   education(data.education);
+  skills(data.skills);
 })
 var child2=document.querySelector("#child2");
 //carrer
@@ -44,4 +45,44 @@ function education(edu){
     tr1+="<tr><td>"+(i+1)+" </td><td>" +edu[i].degree+"</td> <td>"+edu[i].Institudte+" </td><td>"+edu[i].data+" </td></tr>";
     }
     table.innerHTML=tr+tr1;
+}
+//skills
+function skills(skill){
+  var h2=document.createElement("h1");
+  h2.textContent="technical skills";
+  child2.appendChild(h2);
+  var line=document.createElement("hr");
+  child2.appendChild(line);
+  console.log(skill);
+  for(var i=0;skill.length;i++){
+    console.log(skill[i].title);
+    var titleheading=document.createElement("h4");
+    titleheading.textContent=skill[i].title;
+    child2.appendChild(titleheading);
+    var list=document.createElement("ul");
+    child2.appendChild(list);
+    console.log(skill[i].set.length);
+    for (var j = 0;j<skill[i].set.length;j++){
+      var listitem=document.createElement("li");
+      listitem.textContent=skill[i].set[j];
+      list.appendChild(listitem);
+    }
+  }
+}
+//achievements
+function achievements(achievement){
+  var h2=document.createElement("h1");
+  h2.textContent="achievements";
+  child2.appendChild(h2);
+  var line=document.createElement("hr");
+  child2.appendChild(line);
+  var list=document.createElement("ul");
+  child2.appendChild(list);
+  var i=0;
+  while(i<achievement.length){
+   var listItem=document.createElement("li");
+    listeItem.textcontent=achievement[i].achievedData;
+    list.appendChild(listItem);
+    i++;
+  }
 }
